f64c16bb9cb29a1048881b96e51ccea6eeb5c77f
Ali Şahin Özçelik <alisahinozcelik@gmail.com>
2019-08-29 17:23:26 +0300

    fix: ctorParameters option added to client

--- projects/rest/src/lib/client.decorator.ts
+++ projects/rest/src/lib/client.decorator.ts
@@ -1,17 +1,22 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, Injector } from '@angular/core';
+import { Injectable, Injector, Type } from '@angular/core';
 import { INJECTOR, HTTP_CLIENT, BASE_URL, ClientOptions, GUARDS, CLIENT_GUARDS } from './client.interface';
 import { BASE_URL as BASE_URL_TOKEN } from './base-url.token';
 
 
 export function Client<T>({ baseUrl, guards, providedIn = 'root' }: ClientOptions<T> = {}) {
   return function ( Target: new (...args: any[]) => T ): any {
-    const params: any[] = Reflect.getMetadata('design:paramtypes', Target);
+    let params: Type<any>[];
+
+    if ('ctorParameters' in Target) {
+      params = (<{type: Type<any>}[]>Target['ctorParameters'] || []).map(p => p.type);
+    } else {
+      params = Reflect.getMetadata('design:paramtypes', Target) || [];
+    }
 
     class RestClient {
       constructor(injector: Injector) {
-        const _params: any[] = Reflect.getMetadata('design:paramtypes', Target);
-        const newTarget = new (<any>Target)(...(_params || []).map(param => injector.get(param)));
+        const newTarget = new (<any>Target)(...params.map(param => injector.get(param)));
 
         newTarget[INJECTOR] = injector;
         newTarget[HTTP_CLIENT] = injector.get(HttpClient);
@@ -28,7 +33,7 @@ export function Client<T>({ baseUrl, guards, providedIn = 'root' }: ClientOption
 
     Reflect.defineMetadata('design:paramtypes', [Injector], RestClient);
 
-    Injectable({ providedIn, deps: [...(params || [])]})(RestClient);
+    Injectable({ providedIn, deps: [...params]})(RestClient);
 
     return <any>RestClient;
   };
