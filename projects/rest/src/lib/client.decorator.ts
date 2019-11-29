da83e0b3f560439c70a5d1346acd312b884e2659
Ali Şahin Özçelik <alisahinozcelik@gmail.com>
2019-11-30 00:06:21 +0300

    build(Playground): Playground aot testing build conf

--- projects/rest/src/lib/client.decorator.ts
+++ projects/rest/src/lib/client.decorator.ts
@@ -6,66 +6,6 @@ import { INJECTOR, HTTP_CLIENT, BASE_URL, ClientOptions, GUARDS, CLIENT_GUARDS,
           CLIENT_WITH_CREDENTIALS, ON_CLIENT_READY, INJECTIONS } from './types';
 import { BASE_URL as BASE_URL_TOKEN } from './tokens';
 
-// export function Client<T>(
-//   {
-//     baseUrl,
-//     guards,
-//     providedIn,
-//     handlers = [],
-//     baseHeaders = [],
-//     withCredentials
-//   }: ClientOptions<T> = {}
-// ) {
-//   return function(Target: new (...args: any[]) => T): any {
-//     Target[GUARDS] = {
-//       ...Target[GUARDS],
-//       [CLIENT_GUARDS]: guards ? guards instanceof Array ? guards : [guards] : []
-//     };
-
-//     Target[HANDLERS] = {
-//       ...Target[HANDLERS],
-//       [CLIENT_HANDLERS]: [...handlers]
-//     };
-
-//     Target[HEADERS] = {
-//       ...Target[HEADERS],
-//       [CLIENT_HEADERS]: baseHeaders
-//     };
-
-//     if (typeof withCredentials !== undefined) {
-//       Target[WITH_CREDENTIALS] = {
-//         ...Target[WITH_CREDENTIALS],
-//         [CLIENT_WITH_CREDENTIALS]: withCredentials
-//       };
-//     }
-
-//     @Injectable({
-//       providedIn
-//     })
-//     class RestClient {
-//       constructor(injector: Injector) {
-//         const instance = new Target() as any;
-
-//         instance[INJECTOR] = injector;
-//         instance[HTTP_CLIENT] = injector.get(HttpClient);
-//         instance[BASE_URL] = baseUrl || injector.get(BASE_URL_TOKEN);
-
-//         for (const [key, token] of Object.entries(Target[INJECTIONS] || {})) {
-//           instance[key] = injector.get(token);
-//         }
-
-//         if (Target[ON_CLIENT_READY]) {
-//           instance[Target[ON_CLIENT_READY]]();
-//         }
-
-//         return instance;
-//       }
-//     }
-
-//     return Injectable({ providedIn })(RestClient);
-//   };
-// }
-
 export function Client<T>(
   {
     baseUrl,
